55f1feaeb0cdf585cd2d7bae3d61b47c442f8e8b
Mike Pennisi <mike@mikepennisi.com>
2022-11-23 21:34:18 -0500

    Meta: validate CDDL in continous integration
    These GitHub Action Jobs are based on those created for the WebDriver
    Bidi project:
    
    https://github.com/w3c/webdriver-bidi/blob/15580009d02fe33a1123a78662fc4970fda5fd4c/.github/workflows/test.yml

--- scripts/test-cddl.sh
+++ scripts/test-cddl.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 set -ex
 
+SCRIPTS_DIR=$(cd $(dirname "$0") && pwd -P)
+ROOT=$(dirname $SCRIPTS_DIR)
+
 if ! [ -x "$(command -v cddl)" ] || [ "$1" = "--upgrade" ]; then
   echo 'Installing cddl'
   cargo install cddl
 fi
 
-cddl compile-cddl --cddl schemas/at-driver-local.cddl
-cddl compile-cddl --cddl schemas/at-driver-remote.cddl
+cddl compile-cddl --cddl ${ROOT}/schemas/at-driver-local.cddl
+cddl compile-cddl --cddl ${ROOT}/schemas/at-driver-remote.cddl
